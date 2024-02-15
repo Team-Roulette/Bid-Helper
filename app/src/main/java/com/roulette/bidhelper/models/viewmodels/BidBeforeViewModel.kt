349420b4ebb7671959b5a5dab93e091c7df761b6
sw19_sungpil <philip000412@gmail.com>
2024-02-15 21:35:20 +0900

    feat: Add ViewModel(미완)

--- app/src/main/java/com/roulette/bidhelper/models/viewmodels/BidBeforeViewModel.kt
+++ app/src/main/java/com/roulette/bidhelper/models/viewmodels/BidBeforeViewModel.kt
@@ -4,16 +4,14 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class BidBeforeViewModel : ViewModel() {
-    private var mainCategory = MutableLiveData("업종 구분")
-        get() = mainCategory
-    private var firstCategory = MutableLiveData("업종 구분")
-        get() = firstCategory
-    private var secondCategory = MutableLiveData("업종 구분")
-    private var placeCategory = MutableLiveData("지역제한선택")
-    private var dateFrom = MutableLiveData("202401010000")
-    private var dateTo = MutableLiveData("202401012359")
-    private var budgetCategory = MutableLiveData("기초금액")
-    private var budgetMin = MutableLiveData("0")
-    private var budgetMax = MutableLiveData("123456789")
-    private var bidName = MutableLiveData("공고명")
+    val mainCategory = MutableLiveData("업종 구분")
+    val firstCategory = MutableLiveData("업종 구분")
+    val secondCategory = MutableLiveData("업종 구분")
+    val placeCategory = MutableLiveData("지역제한선택")
+    val dateFrom = MutableLiveData("202401010000")
+    val dateTo = MutableLiveData("202401012359")
+    val budgetCategory = MutableLiveData("기초금액")
+    val budgetMin = MutableLiveData("0")
+    val budgetMax = MutableLiveData("123456789")
+    val bidName = MutableLiveData("공고명")
 }
