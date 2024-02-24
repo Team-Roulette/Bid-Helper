c489ebf2cac4d762b5d04ca0f832194e3c8a20ee
Seokjun Lee <seokjun2000@konkuk.ac.kr>
2024-02-24 17:17:01 +0900

    feat: PastInfo LoadingScreen Added
    Loading, Result, Error Screens added

--- app/src/main/java/com/roulette/bidhelper/ui/pastinfo/PastInfoListScreen.kt
+++ app/src/main/java/com/roulette/bidhelper/ui/pastinfo/PastInfoListScreen.kt
@@ -1,12 +1,15 @@
 package com.roulette.bidhelper.ui.pastinfo
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
@@ -14,10 +17,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import com.roulette.bidhelper.R
 import com.roulette.bidhelper.models.apis.BidResultPriceDTO
+import com.roulette.bidhelper.ui.pastinfo.viewmodels.BidResultUiState
 import com.roulette.bidhelper.ui.pastinfo.viewmodels.PastInfoSharedViewModel
 
 @Composable
@@ -26,17 +33,44 @@ fun PastInfoListScreen(
     onItemClicked:() -> Unit,
     modifier: Modifier = Modifier
 ) {
+
+
     val itemList = viewModel.bidResultPrice
+    val bidResultUiState = viewModel.bidResultUiState
+
+    when (bidResultUiState) {
+        is BidResultUiState.Loading -> LoadingScreen(modifier = modifier.fillMaxSize())
+        is BidResultUiState.Success -> ResultScreen(
+            itemList = bidResultUiState.bidResultPriceDTO,
+            modifier = modifier.fillMaxWidth(),
+            onItemClicked = onItemClicked
+        )
+        is BidResultUiState.Error -> LoadingScreen(modifier = modifier.fillMaxSize())
+    }
+}
+
+@Composable
+fun LoadingScreen(modifier: Modifier = Modifier) {
+    Image(
+        modifier = modifier.size(200.dp),
+        painter = painterResource(R.drawable.ic_launcher_foreground),
+        contentDescription = "로딩 중"
+    )
+}
+
+@Composable
+fun ResultScreen(
+    modifier: Modifier = Modifier,
+    onItemClicked: () -> Unit,
+    itemList: BidResultPriceDTO) {
     LazyColumn(
         modifier = modifier) {
-        if(itemList.value?.response?.body?.items != null) {
-            items(itemList.value?.response?.body?.items!!) {
-                ListItem(
-                    item = it,
-                    onItemClicked = onItemClicked
-                )
-                Spacer(modifier = Modifier.fillMaxWidth().height(1.dp).background(Color.LightGray))
-            }
+        items(itemList.response.body.items) {
+            ListItem(
+                item = it,
+                onItemClicked = onItemClicked
+            )
+            Spacer(modifier = Modifier.fillMaxWidth().height(1.dp).background(Color.LightGray))
         }
     }
 }
